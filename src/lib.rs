55e0c46e17efafd289d67b03713d43880360b5f0
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-05-20 11:50:17 -0400

    Refactor: remove unused code and export key modules
    Removed the unused `add` function and its associated tests to streamline the codebase. Exported `Authentication` and `User` modules to enhance reusability and maintain a cleaner structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,5 @@
 mod middleware;
 mod user;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use middleware::Authentication;
+pub use user::User;
